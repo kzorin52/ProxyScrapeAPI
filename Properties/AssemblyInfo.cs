6c2e3edf49de54ca1de0154b56a3ddb01ee3c615
kzorin52 <kzorin52@gmail.com>
2020-12-23 21:25:47 +0600

    Добавьте файлы проекта.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ProxyScrapeAPI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProxyScrapeAPI")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("efdf46ea-50f4-4dfa-99f2-5a907e9036ed")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ProxyScrapeAPI.cs
@@ -0,0 +1,118 @@
+﻿using System.Net;
+using System.Text;
+
+namespace ProxyScrapeAPI
+{
+    public class Scraper
+    {
+        public enum AnonymityType
+        {
+            elite,
+            anonymous,
+            transparent,
+            all
+        }
+
+        public enum ProxyType
+        {
+            Socks4 = '4',
+            Socks4a = 'a',
+            Socks5 = '5',
+            Http = 'h',
+            Https = 's',
+            All = 'A'
+        }
+
+        public enum SSLType
+        {
+            yes,
+            no,
+            all
+        }
+
+        public WebClient Downloader = new WebClient();
+
+
+        public void DownloadProxies(ProxyType? type, string path, int? timeout = null, SSLType? ssl = null,
+            AnonymityType? anonymity = null, int? limit = null, int? age = null, int? port = null,
+            int? averagetimeout = null)
+        {
+            var DownloadLink = new StringBuilder("https://api.proxyscrape.com?request=getproxies?");
+
+
+            if (type != null) DownloadLink.Append($"proxytype={type.ToString().ToLower()}&");
+
+            if (timeout != null) DownloadLink.Append($"timeout={timeout}&");
+
+            if (ssl != null) DownloadLink.Append($"timeout={ssl}&");
+
+            if (anonymity != null) DownloadLink.Append($"anonymity={anonymity}&");
+
+            if (limit != null) DownloadLink.Append($"limit={limit}&");
+
+            if (age != null) DownloadLink.Append($"age={age}&");
+
+            if (port != null && port > 0 && port < 65535) DownloadLink.Append($"port={port}&");
+
+            if (averagetimeout != null) DownloadLink.Append($"averagetimeout={averagetimeout}&");
+
+            Downloader.DownloadFile(DownloadLink.ToString().Substring(0, DownloadLink.Length - 1), path);
+        }
+
+        public string GetProxies(ProxyType? type, int? timeout = null, SSLType? ssl = null,
+            AnonymityType? anonymity = null, int? limit = null, int? age = null, int? port = null,
+            int? averagetimeout = null)
+        {
+            var DownloadLink = new StringBuilder("https://api.proxyscrape.com?request=displayproxies?");
+
+
+            if (type != null) DownloadLink.Append($"proxytype={type.ToString().ToLower()}&");
+
+            if (timeout != null) DownloadLink.Append($"timeout={timeout}&");
+
+            if (ssl != null) DownloadLink.Append($"timeout={ssl}&");
+
+            if (anonymity != null) DownloadLink.Append($"anonymity={anonymity}&");
+
+            if (limit != null) DownloadLink.Append($"limit={limit}&");
+
+            if (age != null) DownloadLink.Append($"age={age}&");
+
+            if (port != null && port > 0 && port < 65535) DownloadLink.Append($"port={port}&");
+
+            if (averagetimeout != null) DownloadLink.Append($"averagetimeout={averagetimeout}&");
+
+            return Downloader.DownloadString(DownloadLink.ToString().Substring(0, DownloadLink.Length - 1));
+        }
+
+        public int AmountProxies(ProxyType? type, int? timeout = null, SSLType? ssl = null,
+            AnonymityType? anonymity = null, int? age = null, int? port = null,
+            int? averagetimeout = null)
+        {
+            var DownloadLink = new StringBuilder("https://api.proxyscrape.com?request=amountproxies?");
+
+
+            if (type != null) DownloadLink.Append($"proxytype={type.ToString().ToLower()}&");
+
+            if (timeout != null) DownloadLink.Append($"timeout={timeout}&");
+
+            if (ssl != null) DownloadLink.Append($"timeout={ssl}&");
+
+            if (anonymity != null) DownloadLink.Append($"anonymity={anonymity}&");
+
+            if (age != null) DownloadLink.Append($"age={age}&");
+
+            if (port != null && port > 0 && port < 65535) DownloadLink.Append($"port={port}&");
+
+            if (averagetimeout != null) DownloadLink.Append($"averagetimeout={averagetimeout}&");
+
+            return int.Parse(Downloader.DownloadString(DownloadLink.ToString().Substring(0, DownloadLink.Length - 1)));
+        }
+
+        public string LastUpdates(ProxyType? type)
+        {
+            return Downloader.DownloadString(
+                $"https://api.proxyscrape.com/?request=lastupdated&proxytype={type.ToString().ToLower()}");
+        }
+    }
+}
